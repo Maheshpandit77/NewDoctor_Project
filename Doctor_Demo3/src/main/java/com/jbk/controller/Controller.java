051abafc30a047bd57c1be3339b3bf7321484f25
rahul wasatkar <rswasatkar25@gmail.com>
2022-11-24 12:26:49 +0530

    Commit DoctorController, Dao_Impl, And Service_Impl to NewBranch

--- Doctor_Demo3/src/main/java/com/jbk/controller/Controller.java
+++ Doctor_Demo3/src/main/java/com/jbk/controller/Controller.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,56 +21,60 @@ import com.jbk.service.doctorService;
 @RequestMapping("/doctor")
 
 public class Controller {
-@Autowired
-private doctorService service;
+	@Autowired
+	private doctorService service;
 
-@PostMapping(value="/saveDoctor")
-public ResponseEntity<Doctor>  saveDoctor(@RequestBody Doctor doctor) {
-	Doctor doct=service.saveDoctor(doctor);
-	return new ResponseEntity<Doctor>(doct,HttpStatus.OK);
-	
-	
-}
-@GetMapping(value="getAllDoctors")
-public ResponseEntity<List<Doctor>>getAllDoctors(){
-	
-	List<Doctor>list=service.getAllDoctors();
-	if(list!=null) {
-	return new ResponseEntity<List<Doctor>>(list,HttpStatus.OK);
-	}else {
-	return new ResponseEntity<List<Doctor>>(HttpStatus.INTERNAL_SERVER_ERROR);
-}
-}
-@GetMapping(value="getDoctorById/{doctorId}")
-public ResponseEntity<Doctor> getDoctorById(@PathVariable Integer doctorId) {
-	 Doctor doctor= service.getDoctorById(doctorId);
-	 if(doctor!=null) {
-		 return new ResponseEntity<Doctor>(doctor,HttpStatus.OK);
-		 
-	 }else
-	 {
+	@PostMapping(value = "/saveDoctor")
+	public ResponseEntity<Doctor> saveDoctor(@RequestBody Doctor doctor) {
+		Doctor doct = service.saveDoctor(doctor);
+		return new ResponseEntity<Doctor>(doct, HttpStatus.OK);
 
-			return new ResponseEntity<Doctor>(HttpStatus.INTERNAL_SERVER_ERROR); 
-	 }
-	
-	
-}
-
-@PutMapping(value="updateDoctor")
-public ResponseEntity<Doctor>updateDoctor(@RequestBody Doctor doctor)
-{
-	Doctor  dr=service.UpdateDoctor(doctor);
-	if(dr!=null) {
-		return new ResponseEntity<Doctor>(dr,HttpStatus.OK);
-	}else {
-		return new  ResponseEntity<Doctor>(HttpStatus.INTERNAL_SERVER_ERROR); 
 	}
-	
-	
-	
-}
 
+	@GetMapping(value = "getAllDoctors")
+	public ResponseEntity<List<Doctor>> getAllDoctors() {
 
+		List<Doctor> list = service.getAllDoctors();
+		if (list != null) {
+			return new ResponseEntity<List<Doctor>>(list, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<List<Doctor>>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 
+	@GetMapping(value = "getDoctorById/{doctorId}")
+	public ResponseEntity<Doctor> getDoctorById(@PathVariable Integer doctorId) {
+		Doctor doctor = service.getDoctorById(doctorId);
+		if (doctor != null) {
+			return new ResponseEntity<Doctor>(doctor, HttpStatus.OK);
+
+		} else {
+
+			return new ResponseEntity<Doctor>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+	}
+
+	@PutMapping(value = "updateDoctor")
+	public ResponseEntity<Doctor> updateDoctor(@RequestBody Doctor doctor) {
+		Doctor dr = service.UpdateDoctor(doctor);
+		if (dr != null) {
+			return new ResponseEntity<Doctor>(dr, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<Doctor>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+	}
+
+	@DeleteMapping(value = "deleteDoctor")
+	public ResponseEntity<Doctor> deleteDoctor(@PathVariable Integer doctorId) {
+		boolean dr = service.deletestudentById(doctorId);
+		if (dr) {
+			return new ResponseEntity<>(HttpStatus.OK);
+		} else {
+			return new ResponseEntity<Doctor>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+	}
 
 }
--- Doctor_Demo3/src/main/java/com/jbk/dao/dao_Impl.java
+++ Doctor_Demo3/src/main/java/com/jbk/dao/dao_Impl.java
@@ -10,107 +10,109 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.jbk.Doctor.Doctor;
+
 @Repository
 public class dao_Impl implements doctorDao {
-@Autowired
-private SessionFactory sf;
+	@Autowired
+	private SessionFactory sf;
+
 	@Override
 	public Doctor saveDoctor(Doctor dactor) {
-		
-		Session session =sf.openSession();
-		Transaction ts=session.beginTransaction();
-		
+
+		Session session = sf.openSession();
+		Transaction ts = session.beginTransaction();
+
 		try {
-		session.save(dactor);
-		ts.commit();
-		}
-		catch(Exception e){
+			session.save(dactor);
+			ts.commit();
+		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
-		}
-		finally {
+		} finally {
 			session.close();
 		}
-		
-		
-		
+
 		return dactor;
 	}
 
 	@Override
 	public List<Doctor> getAllDoctors() {
-		Session session=sf.openSession();
-		 Criteria criteria=session.createCriteria(Doctor.class);
-		 List<Doctor>list=null;
-		 try 
-		 {
-			list= criteria.list();
-		 }catch(Exception e) 
-		 {
-			e.printStackTrace(); 
+		Session session = sf.openSession();
+		Criteria criteria = session.createCriteria(Doctor.class);
+		List<Doctor> list = null;
+		try {
+			list = criteria.list();
+		} catch (Exception e) {
+			e.printStackTrace();
 			return null;
-		 }
-		 finally 
-		 {
-			session.close(); 
-		 }
+		} finally {
+			session.close();
+		}
 		return list;
 	}
 
 	@Override
 	public Doctor getDoctorById(Integer doctorId) {
-		Session session =sf.openSession();
-		Doctor doctor=null;
+		Session session = sf.openSession();
+		Doctor doctor = null;
 		try {
-		doctor=	session.get(Doctor.class, doctorId);
-		}
-		catch(Exception e)
-		{
+			doctor = session.get(Doctor.class, doctorId);
+		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
-		}
-		finally {
+		} finally {
 			session.close();
 		}
-		
+
 		return doctor;
 	}
 
 	@Override
 	public Doctor UpdateDoctor(Doctor doctor) {
-		Session session =sf.openSession();
-		Criteria criteria=session.createCriteria(Doctor.class);
-		Transaction tr=session.beginTransaction();
-		List<Doctor>list=null;
+		Session session = sf.openSession();
+		Criteria criteria = session.createCriteria(Doctor.class);
+		Transaction tr = session.beginTransaction();
+		List<Doctor> list = null;
 		try {
-			list= criteria.list();
-			for(Doctor dt:list)
-			if(dt.getDoctorId()==doctor.getDoctorId()) {
-				list.remove(dt);
-				list.add(doctor);
-				tr.commit();
-			}}
-				
-			
-			catch (Exception e) {
-				e.printStackTrace();
-				return null;
-			}finally {
-				session.close();
-			}
-			return doctor;
-				
-			}
-		
-		
-		
-		
-	
+			list = criteria.list();
+			for (Doctor dt : list)
+				if (dt.getDoctorId() == doctor.getDoctorId()) {
+					list.remove(dt);
+					list.add(doctor);
+					tr.commit();
+				}
+		}
+
+		catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		} finally {
+			session.close();
+		}
+		return doctor;
+
+	}
 
 	@Override
 	public boolean deletestudentById(Integer doctorId) {
-		// TODO Auto-generated method stub
-		return false;
+
+		Session session = sf.openSession();
+		Transaction tr = session.beginTransaction();
+		boolean isdeleted = false;
+		try {
+			Doctor dr = session.get(Doctor.class, doctorId);
+			if (dr != null) {
+				session.delete(dr);
+				tr.commit();
+				isdeleted = true;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			session.close();
+		}
+
+		return isdeleted;
 	}
 
 }
